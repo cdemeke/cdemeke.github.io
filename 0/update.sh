62dd0262c74b5a726db9ab629c4f13c2d68e0e20
Chris Demeke <demeke2@gmail.com>
2015-05-08 23:53:25 -0400

    updated blog

---
+++ 0/update.sh
@@ -0,0 +1,21 @@
+# Bash script to update update the static version of the ghost site
+buster generate --domain=http://127.0.0.1:2368
+
+# Delete existing Ghost static folder 
+rm -rvf static/0/ghost
+
+# Delete update script from static folder
+rm -rvf static/0/update.sh
+# Copy the Ghost version of the site into the static folder
+#mkdir static/0/ghost
+cp -a ghost static/0/ghost
+
+# Copy this script into the static folder
+cp update.sh static/0
+
+# go into static folder
+cd static
+
+# add all files to github
+git add *
+echo "Ready to commit and add to repo"
