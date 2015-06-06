00a5525f3acd6436602b438755aa8af8b0295d2a
Chris Demeke <demeke2@gmail.com>
2015-06-05 22:57:47 -0400

    updated with merged homepage

---
+++ js/custom.js
@@ -0,0 +1,39 @@
+//finds    the assoicated wrapper
+function findCorrectWrap(id) {
+  return $('.'+id+'.wrap')
+ //$('.'+id+'.wrap').toggleClass('active');
+};
+
+//toggles the wrap activate class
+function toggleCorrectWrap(id) {
+  $(id).toggleClass('active');
+}
+
+//add hide class
+function hideThis() {
+    setTimeout(function () {
+        $('.wrap').addClass('hide');
+    }, 1000);
+}
+
+//listener for a button to be pressed
+$('.buttons').on('click', function(e) {
+  $('.wrap').removeClass('hide');
+  //get the exact location of the mouse click
+  //alert(e.pageX+ ' , ' + e.pageY);
+  //grabs the value attribute from the clicked button
+  var id = $(this).attr("value");
+  //class function to find assoicated wrapper
+  var wrapper = findCorrectWrap(id)
+  
+  //checks if the the wrap div is already activated
+  if ($(wrapper).hasClass('active')) {
+    }
+    else{
+      //hides all active wraps
+      $('.wrap').removeClass('active');
+    };
+    //calls function to activate the assoicated wrapper
+    toggleCorrectWrap(wrapper);
+
+});
